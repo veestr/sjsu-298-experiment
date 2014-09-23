cc07ecb59d8a4ee5cb8533aac6566f103c39b64e
veestr <v.struchkov@gmail.com>
2014-09-22 22:25:04 -0700

    Made reports pretty
    Time shows the timestamp in PST.
    Cache is disabled for /report.

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ import glob
 import json
 import cgi
 import string
-import datetime
+
 import re
+import time
+
+from datetime import datetime, tzinfo, timedelta
 
 from google.appengine.ext import db
 from google.appengine.api import memcache
@@ -38,8 +41,28 @@ report_template=JINJA_ENVIRONMENT.get_template('report.html')
 account_template=JINJA_ENVIRONMENT.get_template('account.html')
 bad_password_template=JINJA_ENVIRONMENT.get_template('bad_password.html')
 
+
+class Zone(tzinfo):
+	"""Define timezone objects."""
+	def __init__(self,offset,isdst,name):
+		self.offset = offset
+		self.isdst = isdst
+		self.name = name
+		
+	def utcoffset(self, dt):
+		return timedelta(hours=self.offset) + self.dst(dt)
+	
+	def dst(self, dt):
+		return timedelta(hours=1) if self.isdst else timedelta(0)
+	
+	def tzname(self,dt):
+		return self.name
+
+PST = Zone(-8, True, 'PST')
+
 class Account(db.Model):
-	date = db.DateTimeProperty(auto_now_add=True)
+	"""Defines accounts in the database."""
+	date = db.StringProperty()
 	user = db.StringProperty(indexed=True)
 	site = db.StringProperty()
 	initial_password = db.StringProperty()
@@ -67,8 +90,9 @@ def get_all_accounts():
 def get_possible_sites():
 	"""Returns a set of sites available for account creation. Each site 
 	is represented by an image in the 'images' directory. """
-	files=set(glob.glob('images/*'))
-	return files
+	files=(file.replace('images/', '') for file in glob.glob('images/*'))
+	#files=set(glob.glob('images/*'))
+	return set(files)
 	
 def get_registered_sites(user,iteration):
 	"""Returns a set of the sites the specified user has registered for given the 
@@ -101,6 +125,19 @@ def verify_site(user, site, password):
 	stored_pass = str(result.initial_password)
 	return stored_pass == password
 	
+def get_site_for_known_user(iteration, user):
+	"""Returns the site for the specified user or '' of no such sites can be returned."""
+	possible_sites=get_possible_sites()
+	last_site=memcache.get(user)
+	registered_sites=get_registered_sites(user,iteration)
+	registered_sites.add(last_site)
+	allowed_sites=possible_sites.difference(registered_sites)
+	print registered_sites, possible_sites
+	if len(allowed_sites) > 0:
+		return random.sample(allowed_sites, 1).pop()
+	else:
+		return ''
+			
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		self.response.write(main_template.render())
@@ -117,7 +154,7 @@ class ReportHandler(webapp2.RequestHandler):
 		
 class AccountHandler(webapp2.RequestHandler):
 	
-	def get(self, iteration,attempt):
+	def get(self,iteration,attempt):
 		user=cgi.escape(self.request.get('user'))
 		site=cgi.escape(self.request.get('site'))
 		possible_sites=get_possible_sites()		
@@ -129,14 +166,11 @@ class AccountHandler(webapp2.RequestHandler):
 					#ASSESRT: We know for which site to display the account info
 					selected_site = site
 				else:
-					#ASSERT: We must choose the site
-					last_site=memcache.get(user)
-					registered_sites=get_registered_sites(user,iteration)
-					registered_sites.add(last_site)
-					allowed_sites=possible_sites.difference(registered_sites)
-					if len(allowed_sites)==0:
+					#ASSERT: we need to figure out the site for the user and if
+					# such site does not exist, we need to go back to the main screen
+					selected_site=get_site_for_known_user(iteration, user)
+					if selected_site=="":
 						self.redirect('/')
-					selected_site=random.sample(allowed_sites, 1).pop()
 			else:
 				#ASSERT: We have not seen this user before. Provide a list
 				# of sites without any restrictions
@@ -175,7 +209,8 @@ class AccountHandler(webapp2.RequestHandler):
 			initial_password=password,
 			site=site,
 			second_password=-1,
-			third_password=-1,	
+			third_password=-1,
+			date=datetime.now(PST).strftime('%m/%d/%Y %H:%M:%S %Z')
 			)
 		account.put()	
 		memcache.set(key=user, value=site)
@@ -240,6 +275,7 @@ app = webapp2.WSGIApplication([
     webapp2.Route(r'/', handler=MainHandler),
 	webapp2.Route(r'/report', handler=ReportHandler),
 	webapp2.Route(r'/account/<iteration>/<attempt>/', handler=AccountHandler),
+	# webapp2.Route(r'/account/<iteration>/<attempt>/<user>', handler=AccountHandler, handler_method='handle_known_user'),
 	webapp2.Route(r'/save',   handler=AccountHandler, methods=['POST'], handler_method='save'),
 	webapp2.Route(r'/verify', handler=AccountHandler, methods=['POST'], handler_method='verify')
 ], debug=True)
